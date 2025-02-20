6511edd8f07dc219bb2a7adb3f9177c27b77ce8b
BelchaV <32213019+BelchaV@users.noreply.github.com>
2025-02-19 23:35:37 -0300

    Vers 3

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//#endregion
-
+//JS para dinamizar el juego
+//Para saber cuantos amigos se ingresaron. Una primer idea para que una Pantalla maso....
 let intentos = 0;
-let limitador = 3;
+//Variable que controla la cantidad de amigos ingresados. Y se usa para el calc aleatorio obvio
+let limitador = 5;
 let lista_amigos = [];
 
+//Funcion para limpiar la caja (unica por ahora) que tenemos
 function limpiarCaja() {
     document.querySelector('#nbreAmigo').value = '';
     document.querySelector('#nbreAmigo').focus();
 }
 
+//Otra funcion que es interesante como para usar en otros programas
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+//Cada vez que el Sorteo se hace, reinciamos el juego con esta funciòn
 function condicionesIniciales() {
-    //asignarTextoElemento('h1','Juego del número secreto!');
-    //asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
-    //numeroSecreto = generarNumeroSecreto();
     console.log(intentos);
     intentos = 0;
     lista_amigos = [];
+    //advertimos el limitador (dinámico...hasta ahí) que pusimos
+    asignarTextoElemento('p',`(se pueden ingresar hasta ${limitador} amigos)`);
 }
 
+//Asociada al boton de Sorteo
 function sortearAmigo() {
     //
     cant_elem = lista_amigos.length,
@@ -30,54 +39,41 @@ function sortearAmigo() {
     condicionesIniciales();
 }
 
-
+//Asociada al boton para Añadir un amigo nuevo
 function agregarAmigo() {
     //esto lo coloco por si se juega por segunda vez. Sinó habría que poner otro boton
     if (intentos == 0) {
         document.getElementById("resultado").innerHTML = "";
     }
-    intentos++;
-    if (intentos > limitador) {
-        window.alert("No se puede agregar mas Amigos. Ir a Sortear")
-        return
-    }
     let nbre_amigo = document.getElementById('nbreAmigo').value;
-    //Si el numero generado está incluido en la lista 
+    //Si el nombre de amigo ingresado está incluido en la lista 
     if (typeof nbre_amigo === "string" && nbre_amigo.length === 0) {
         window.alert("Ingrese texto válido")
         return
     }
+    //Si se llegó al Limite de amigos
+    if (intentos == limitador) {
+        window.alert("No se puede agregar mas Amigos. Ir a Sortear")
+        limpiarCaja();
+        return
+    }
     if (lista_amigos.includes(nbre_amigo)) {
         window.alert("ya existe ese amigui")
-        //return generarNumeroSecreto();
         } else {
+        intentos++;
         let lista_amostrar = document.getElementById("listaAmigos");
         let nuevo_amigo = document.createElement("li");
         nuevo_amigo.textContent = nbre_amigo;
         nuevo_amigo.setAttribute("role", "listitem");
         lista_amostrar.appendChild(nuevo_amigo);
+        //agrega el amigo ingresado a la Lista creada
         lista_amigos.push(nbre_amigo);
         console.log(nbre_amigo);
+        //borra la caja del Input
         limpiarCaja();
-        if (intentos == limitador) {
-            window.alert(`Es el último amigo. Llegaste a ${limitador}`)
-            return
-        }
-        //return numeroGenerado;
+        //if (intentos == limitador) {
+        //    window.alert(`Es el último amigo. Llegaste a ${limitador}`)
+        //}
     }
 }
-
-
-function reiniciarJuego() {
-    //limpiar caja
-    limpiarCaja();
-    //Indicar mensaje de intervalo de números 
-    //Generar el número aleatorio
-    //Inicializar el número intentos
-    condicionesIniciales();
-    //Deshabilitar el botón de nuevo juego
-    //document.querySelector('#reiniciar').setAttribute('disabled','true');
-    
-}
-
 condicionesIniciales();
